af54a9360c22034e162dda00c7720dc595618744
Alireza-Azizi <35219628+alrzazz@users.noreply.github.com>
2019-11-04 01:48:46 +0330

    Deploy

---
+++ precache-manifest.605f6e5ba6233d205d054ff18d4a9909.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241e6fc010879ffe4d78a4ccca0d166f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d285fde8193aff0f4d6",
+    "url": "/static/css/main.eb81190c.chunk.css"
+  },
+  {
+    "revision": "0d87127291926b9c9c9b",
+    "url": "/static/js/2.e86980dc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e86980dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d285fde8193aff0f4d6",
+    "url": "/static/js/main.cdfec852.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "b54724f54d4dd3f6796e3c4cc422f998",
+    "url": "/static/media/GothamBook.b54724f5.ttf"
+  },
+  {
+    "revision": "58f6d7472cc08a4f9b963d6402929342",
+    "url": "/static/media/avatar.58f6d747.jpg"
+  },
+  {
+    "revision": "b53ff55e09a092f9f0cbbf74af01b715",
+    "url": "/static/media/github-logo.b53ff55e.svg"
+  },
+  {
+    "revision": "c1eb27404bbc0d6052620ac79194ae19",
+    "url": "/static/media/linkedin.c1eb2740.svg"
+  },
+  {
+    "revision": "a888fec0a2859e59674a89bc7722e062",
+    "url": "/static/media/telegram.a888fec0.svg"
+  }
+]);
