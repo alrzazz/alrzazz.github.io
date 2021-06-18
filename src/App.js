246cbc70d7cf79175a988fd07f24c8b5a8a81e5d
Alireza <alirezaazizi1396622@gmail.com>
2021-06-18 15:38:55 +0430

    Add v2

--- src/App.js
+++ src/App.js
@@ -1,38 +1,183 @@
 import React, { Component } from "react";
 import "./App.css";
-import MainContent from "./MainContent";
-import Sider from "./Sider";
+import { Spring, useSpring, animated } from "react-spring";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faTelegram,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import { faDownload, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Card from "./components/Card";
 
 class App extends Component {
   render() {
     return (
-      <div style={{ height: "100vh" }}>
-        <div className="p-4">
-          <img
-            src={require("./static/avatar.jpg")}
-            alt="avatar"
-            style={{
-              width: "150px",
-              borderWidth: "2px",
-              borderColor: "gray",
-              borderStyle: "solid",
-              borderRadius: "10%",
+      <div>
+        <div
+          style={{
+            height: "100vh",
+            position: "static",
+            zIndex: -1,
+            width: "100vw",
+            // backgroundImage: `url(${require("./assets/back.jpg")})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div
+          style={{
+            height: "100vh",
+            top: 0,
+            position: "absolute",
+          }}
+        >
+          <Spring
+            delay={500}
+            from={{
+              opacity: 0,
+              marginTop: -1000,
+              height: "100vh",
+              backgroundColor: "rgba(0, 0, 0, 0.85)",
             }}
-          />
-          <div className="p-3">
-            <div>
-              <h1>Alireza Azizi</h1>
-              <h4>Born 1998</h4>
-              <p>
-                Backend developer interested in Deep Learning and Artificial
-                Intelligence
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="main">
-          <Sider />
-          <MainContent />
+            to={{
+              opacity: 1,
+              marginTop: 0,
+              height: "100vh",
+              backgroundColor: "rgba(0, 0, 0, 0.85)",
+            }}
+          >
+            {(styles) => (
+              <animated.div style={styles}>
+                <div
+                  style={{
+                    textAlign: "center",
+                    color: "white",
+                    width: "60%",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    paddingTop: "100px",
+                  }}
+                >
+                  <h1 className="mb-4">Alireza Azizi</h1>
+                  <img
+                    src={require("./assets/avatar.jpg")}
+                    className="mb-4"
+                    style={{
+                      borderRadius: "2rem",
+                      width: "300px",
+                      height: "300px",
+                    }}
+                  />
+                  <p className="mb-4">
+                    Hi, I'm so happy for see you there. My name is Alireza and
+                    I'm a Mid Level Backend Developer and AI Enthusiast.
+                    Currently study Computer Engingeering in Iran University of
+                    Science and Technology and doing my project from home. Glad
+                    to keep in touch with me.
+                  </p>
+                  <div
+                    className="d-flex flex-row justify-content-center mx-auto mb-4"
+                    style={{
+                      backgroundColor: "white",
+                      width: "fit-content",
+                      padding: "20px",
+                      borderRadius: "2rem",
+                    }}
+                  >
+                    <Card>
+                      <a
+                        data-toggle="tooltip"
+                        data-placement="bottom"
+                        title="Linkedin"
+                        href="https://www.linkedin.com/in/alireza-azizi-8a53521b4/"
+                      >
+                        <FontAwesomeIcon
+                          icon={faLinkedin}
+                          size="5x"
+                          color="#0077b5"
+                          style={{
+                            padding: "10px",
+                          }}
+                        />
+                      </a>
+                    </Card>
+
+                    <Card>
+                      <a
+                        data-toggle="tooltip"
+                        data-placement="bottom"
+                        title="Github"
+                        href="https://github.com/alrzazz/"
+                      >
+                        <FontAwesomeIcon
+                          icon={faGithub}
+                          size="5x"
+                          color="black"
+                          style={{
+                            padding: "10px",
+                          }}
+                        />
+                      </a>
+                    </Card>
+                    <Card>
+                      <a
+                        data-toggle="tooltip"
+                        data-placement="bottom"
+                        title="Telegram"
+                        href="https://t.me/alrzazz"
+                      >
+                        <FontAwesomeIcon
+                          icon={faTelegram}
+                          size="5x"
+                          color="#0088cc"
+                          style={{
+                            padding: "10px",
+                          }}
+                        />
+                      </a>
+                    </Card>
+                    <Card>
+                      <a
+                        data-toggle="tooltip"
+                        data-placement="bottom"
+                        title="Resume"
+                        href="https://raw.githubusercontent.com/alrzazz/CV/6d477f8b7e52beaefc34ef13ae97293d4f8084b7/cv.pdf"
+                        target="_blank"
+                      >
+                        <FontAwesomeIcon
+                          icon={faDownload}
+                          size="5x"
+                          color="#44dd33"
+                          style={{
+                            padding: "10px",
+                          }}
+                        />
+                      </a>
+                    </Card>
+                    <Card>
+                      <a
+                        data-toggle="tooltip"
+                        data-placement="bottom"
+                        title="Gmail"
+                        href="mailto:alirezaazizi1396622@gmail.com"
+                        target="_blank"
+                      >
+                        <FontAwesomeIcon
+                          icon={faEnvelope}
+                          size="5x"
+                          color="red"
+                          style={{
+                            padding: "10px",
+                          }}
+                        />
+                      </a>
+                    </Card>
+                  </div>
+                </div>
+              </animated.div>
+            )}
+          </Spring>
         </div>
       </div>
     );
---
+++ src/components/Card.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useSpring, animated, config } from "@react-spring/web";
+
+const calc = (x, y, rect) => [
+  -(y - rect.top - rect.height / 2) / 5,
+  (x - rect.left - rect.width / 2) / 5,
+  1.2,
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+export default function Card({ children }) {
+  const configList = Object.keys(config);
+  const ref = React.useRef(null);
+  const [xys, set] = React.useState([0, 0, 1]);
+  const props = useSpring({ xys, config: config["default"] });
+
+  return (
+    <div className="ccard-main" ref={ref}>
+      <animated.div
+        className="ccard"
+        style={{ transform: props.xys.to(trans) }}
+        onMouseLeave={() => set([0, 0, 1])}
+        onMouseMove={(e) => {
+          const rect = ref.current.getBoundingClientRect();
+          set(calc(e.clientX, e.clientY, rect));
+        }}
+      >
+        {children}
+      </animated.div>
+    </div>
+  );
+}
