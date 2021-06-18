ac50c8617bf1e99dbc2180ae5174e85bc91cfebc
Alireza <alirezaazizi1396622@gmail.com>
2021-06-18 15:40:44 +0430

    Deploy v2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.605f6e5ba6233d205d054ff18d4a9909.js"
+  "/precache-manifest.f6bed4468d76697233d2e8572c0b497d.js"
 );
 
 self.addEventListener('message', (event) => {
